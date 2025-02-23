6e477fb0937b8d018d14e8ec4e1434848149dc30
luis <code.lucho33@github.com>
2025-02-23 15:12:27 -0500

    Implementación de Gestión de Productos - Añadido ProductService con lógica de negocio para:   - Añadir productos, verificando categorías existentes o creando nuevas.   - Obtener productos por ID con manejo de excepciones (ProductNotFoundException).   - Actualizar productos utilizando UpdateProductRequest para desacoplar la entidad Product.   - Eliminar productos con validación previa de existencia.   - Recuperar productos con filtros flexibles (categoría, marca, nombre, combinaciones). - Añadidos DTOs (AddProductRequest y UpdateProductRequest) para transportar datos entre capa de presentación y servicio. - Creado ProductNotFoundException para manejo específico de errores. - Implementados métodos personalizados en ProductRepository y CategoryRepository:   - findByCategoryName(), findByBrand(), findByCategoryNameAndBrand(), findByName(), findByBrandAndName(), countByBrandAndName(). - Uso de Optional y manejo de excepciones mejorado en todas las operaciones. - Se utilizó Lombok (@Data y @RequiredArgsConstructor) para reducir el boilerplate en DTOs y Servicios.
    Este commit mejora la estructura de la capa de servicio y repositorio, permitiendo una gestión flexible y escalable de productos.

---
+++ src/main/java/com/example/dreamshop/service/product/IProductService.java
@@ -0,0 +1,24 @@
+package com.example.dreamshop.service.product;
+
+import com.example.dreamshop.model.Product;
+import com.example.dreamshop.request.AddProductRequest;
+import com.example.dreamshop.request.UpdateProductRequest;
+
+import java.util.List;
+
+public interface IProductService {
+    //Methodts to comunicate with DB
+    Product addProduct(AddProductRequest product);
+    Product getProductById(Long id);
+    void deleteProductById(Long id);
+    Product updateProduct(UpdateProductRequest request, Long productId);
+    List<Product> getAllProducts();
+    List<Product> getProductsByCategory(String category);
+    List<Product> getProductsByBrand(String brand);
+    List<Product> getProductsByCategoryAndBrand(String category, String brand);
+    List<Product> getProductsByName(String name);
+    List<Product> getProductsByBrandAndName(String brand, String name);
+    Long countProductsByBrandAndName(String brand, String name);
+
+
+}
