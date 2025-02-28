5f1b460a2a00ff58956dce85f0ae82fe6406f0ed
luis <code.lucho33@github.com>
2025-02-28 11:08:06 -0500

    fix: refrescar card item

--- src/main/java/com/example/dreamshop/model/Cart.java
+++ src/main/java/com/example/dreamshop/model/Cart.java
@@ -22,8 +22,7 @@ public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Version
-    private Integer version;
+
     private BigDecimal totalAmount = BigDecimal.ZERO;
     @OneToMany(mappedBy = "cart",cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<CartItem> items = new HashSet<>();
